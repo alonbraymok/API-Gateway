1542479f9985a902df0699be14aa8ea696b6d097
Alon Braymok <alon@ludus.ac>
2020-05-03 17:09:22 +0300

    first commit

--- app.js
+++ app.js
@@ -1,29 +1,21 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
+const gateway = require("fast-gateway");
+const PORT = process.env.PORT || 8080;
 
-const cors = require("cors");
+gateway({
+  middlewares: [require("cors")(), require("helmet")()],
 
-const graphqlHttp = require("express-graphql");
-
-const graphqlSchema = require("./graphql/schema");
-const graphqlResolvers = require("./graphql/resolvers");
-
-const { DB_CONNECT } = require("./config");
-
-const app = express();
-
-app.use(cors());
-app.use(bodyParser.json()); // application/json
-app.use(
-  "./graphql",
-  graphqlHttp({
-    schema: graphqlSchema,
-    rootValue: graphqlResolvers,
-  })
-);
-
-mongoose
-  .connect(DB_CONNECT)
-  .then(app.listen(8081))
-  .catch((err) => console.log(err));
+  routes: [
+    {
+      prefix: "/user",
+      target: "http://localhost:8081",
+    },
+    {
+      prefix: "/task",
+      target: "http://localhost:8082",
+    },
+  ],
+})
+  .start(PORT)
+  .then((server) => {
+    console.log(`API Gateway listening on ${PORT} port!`);
+  });
